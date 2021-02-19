a79ca993f9a06bc03420499fd56662f4844fc511
Jeffrey Oliver <jcoliver@arizona.edu>
2021-02-19 13:52:41 -0700

    Initial concrete Jupyter Notebook

--- concrete-strength.R
+++ concrete-strength.R
@@ -12,6 +12,9 @@ library(tidyr)
 # Read in concrete data
 concrete <- read.csv(file = "data/Concrete_Data.csv")
 
+# Some EDA, looking for good relationships
+pairs(concrete)
+
 # Plot strength vs. age, using OLS linear regression
 ggplot(data = concrete, mapping = aes(x = Age, 
                                       y = Compressive_Strength)) +
@@ -30,3 +33,21 @@ ggplot(data = concrete, mapping = aes(x = Age,
                                       color = Cement)) +
   geom_point(size = 2) +
   geom_smooth(method = "loess")
+
+# What about a polynomial relationship?
+ggplot(data = concrete, mapping = aes(x = Age, 
+                                      y = Compressive_Strength,
+                                      color = Cement)) +
+  geom_point(size = 2) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 4))
+
+
+single <- lm(Compressive_Strength ~ Age, data = concrete)
+order_2 <- lm(Compressive_Strength ~ poly(Age, 2), data = concrete)
+order_3 <- lm(Compressive_Strength ~ poly(Age, 3), data = concrete)
+# Stop at order 4
+order_4 <- lm(Compressive_Strength ~ poly(Age, 4), data = concrete)
+order_5 <- lm(Compressive_Strength ~ poly(Age, 5), data = concrete)
+
+anova(order_3, order_4)
+anova(order_4, order_5)
